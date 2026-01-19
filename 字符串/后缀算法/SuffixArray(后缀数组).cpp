98a78ff27ad2db5b2f154e0f4b24d91c8043e264
Aiza Lee <lee.aizazia@gmail.com>
2026-01-19 12:12:46 +0800

    update suffix array

--- 字符串/后缀算法/SuffixArray(后缀数组).cpp
+++ 字符串/后缀算法/SuffixArray(后缀数组).cpp
@@ -4,11 +4,14 @@
  * Suffix Array (SA) 后缀数组
  * 
  * interface:
- * 		init(string s)					// 初始化后缀数组
- * 		lcp(i, j)						// 求排名为i和j的后缀的最长公共前缀长度
- * 		lcp_pos(p1, p2)					// 求原串位置p1和p2开始的后缀的LCP (p1, p2为1-based索引)
- * 		count_distinct_substrings()		// 计算本质不同子串个数
- * 		find_substring(string sub)		// 查找子串sub在原串中的出现位置范围 [L, R] (在sa数组中的下标范围)
+ * 		SuffixArray(string s)			// 构造函数，O(N log N)
+ * 		lcp(i, j)						// 求排名为i和j的后缀的最长公共前缀长度 O(1)
+ * 		lcp_pos(p1, p2)					// 求原串位置p1和p2开始的后缀的LCP (p1, p2为1-based索引) O(1)
+ * 		count_distinct_substrings()		// 计算本质不同子串个数 O(N)
+ * 		find_substring(string sub)		// 查找子串sub在原串中的出现位置范围 [L, R] (在sa数组中的下标范围) O(|sub| * log N)
+ * 		find_substring(pos, len)		// 查找原串子串str[pos...pos+len-1]在后缀数组中的范围 [L, R] O(log N)
+ * 		compare_substrings(p1, len1, p2, len2) // 比较两个子串的大小 O(1)
+ * 		kth_distinct_substring(k)		// 求第k小(字典序)的本质不同子串 O(N)
  * 
  * note:
  * 		1. sa[i]: 排名为i的后缀的起始位置 (1-based, 值域 1~n)
@@ -25,35 +28,31 @@ struct SuffixArray {
 	std::vector<std::vector<int>> st;
 	std::vector<int> lg;
 
-	void init(const std::string& _str) {
-		n = _str.length();
-		str = " " + _str; // 1-based
-		m = 127;
-		
-		// Resize vectors. rk and oldrk need extra space for doubling
-		sa.assign(std::max(n, m) + 1, 0);
-		rk.assign(2 * n + 5, 0);
-		oldrk.assign(2 * n + 5, 0);
-		id.assign(n + 1, 0);
-		height.assign(n + 1, 0);
-		cnt.assign(std::max(n, m) + 1, 0);
+	void _radix_pass(const std::vector<int>& keys, const std::vector<int>& items, int limit) {
+		std::fill(cnt.begin(), cnt.begin() + limit + 1, 0);
+		rep(i, 1, n) cnt[keys[items[i]]]++;
+		rep(i, 1, limit) cnt[i] += cnt[i - 1];
+		per(i, n, 1) sa[cnt[keys[items[i]]]--] = items[i];
+	}
 
+	SuffixArray(const std::string& _str) : 
+		n(_str.length()), m(127), 
+		sa(std::max(n, m) + 1), rk(2 * n + 5), oldrk(2 * n + 5), 
+		id(n + 1), height(n + 1), cnt(std::max(n, m) + 1),
+		str(" " + _str) 
+	{
 		rep(i, 1, n) {
 			rk[i] = str[i];
-			cnt[rk[i]]++;
+			id[i] = i;
 		}
-		rep(i, 1, m) cnt[i] += cnt[i - 1];
-		per(i, n, 1) sa[cnt[rk[i]]--] = i;
+		_radix_pass(rk, id, m);
 
 		for (int w = 1, p = 0; p < n; w <<= 1, m = p) {
 			p = 0;
 			rep(i, n - w + 1, n) id[++p] = i;
 			rep(i, 1, n) if (sa[i] > w) id[++p] = sa[i] - w;
 
-			std::fill(cnt.begin(), cnt.begin() + m + 1, 0);
-			rep(i, 1, n) cnt[rk[id[i]]]++;
-			rep(i, 1, m) cnt[i] += cnt[i - 1];
-			per(i, n, 1) sa[cnt[rk[id[i]]]--] = id[i];
+			_radix_pass(rk, id, m);
 
 			std::copy(rk.begin(), rk.begin() + n + 1, oldrk.begin());
 			p = 0;
@@ -103,7 +102,6 @@ struct SuffixArray {
 		}
 	}
 
-	// LCP of suffix with rank x and rank y
 	int lcp(int x, int y) {
 		if (x == y) return n - sa[x] + 1;
 		if (x > y) std::swap(x, y);
@@ -112,7 +110,6 @@ struct SuffixArray {
 		return std::min(st[k][x], st[k][y - (1 << k) + 1]);
 	}
 
-	// LCP of suffix starting at p1 and p2 (1-based)
 	int lcp_pos(int p1, int p2) {
 		return lcp(rk[p1], rk[p2]);
 	}
@@ -125,45 +122,66 @@ struct SuffixArray {
 		return ans;
 	}
 
+	template<typename F>
+	std::pair<int, int> _search(F cmp) {
+		int l = 1, r = n, lb = n + 1;
+		while (l <= r) {
+			int mid = l + r >> 1;
+			if (cmp(mid) >= 0) lb = mid, r = mid - 1;
+			else l = mid + 1;
+		}
+
+		l = 1, r = n;
+		int ub = n + 1;
+		while (l <= r) {
+			int mid = l + r >> 1;
+			if (cmp(mid) > 0) ub = mid, r = mid - 1;
+			else l = mid + 1;
+		}
+		return {lb, ub - 1};
+	}
+
 	// 查找子串sub在原串中的出现位置范围 [L, R] (在sa数组中的下标范围)
 	// 返回 {lb, rb}，表示 sa[lb...rb] 为所有以 sub 为前缀的后缀
 	// 如果未找到，返回 {n + 1, n} (即 lb > rb)
 	std::pair<int, int> find_substring(const std::string& sub) {
 		int len = sub.length();
-		
-		// Lower bound: 找到第一个 suffix >= sub
-		int l = 1, r = n, lb = n + 1;
-		while (l <= r) {
-			int mid = l + (r - l) / 2;
-			// str is 1-based, sa[mid] is index in str.
-			// str.compare(pos, count, s2) -> compares substring starting at pos with s2
-			// str has a leading space, so actual string starts at index 1.
-			// sa[mid] is index.
-			// We need to compare str.substr(sa[mid], len) with sub.
-			// str.compare(sa[mid], len, sub)
-			int res = str.compare(sa[mid], len, sub);
-			if (res >= 0) {
-				lb = mid;
-				r = mid - 1;
-			} else {
-				l = mid + 1;
-			}
-		}
+		return _search([&](int mid) {
+			return str.compare(sa[mid], len, sub);
+		});
+	}
 
-		// Upper bound: 找到第一个 suffix > sub (strictly, not prefix match)
-		l = 1, r = n;
-		int ub = n + 1;
-		while (l <= r) {
-			int mid = l + (r - l) / 2;
-			int res = str.compare(sa[mid], len, sub);
-			if (res > 0) {
-				ub = mid;
-				r = mid - 1;
-			} else {
-				l = mid + 1;
-			}
-		}
+	// 查找原串子串str[pos...pos+len-1]在后缀数组中的范围 [L, R]
+	// pos为1-based索引
+	std::pair<int, int> find_substring(int pos, int len) {
+		return _search([&](int mid) {
+			int l = lcp_pos(sa[mid], pos);
+			if (l >= len) return 0;
+			if (sa[mid] + l > n) return -1; // ending earlier, so smaller
+			return str[sa[mid] + l] < str[pos + l] ? -1 : 1;
+		});
+	}
 
-		return {lb, ub - 1};
+	// 比较两个子串的大小 (1-based 索引)
+	// 返回 -1 (s1 < s2), 0 (s1 == s2), 1 (s1 > s2)
+	int compare_substrings(int p1, int len1, int p2, int len2) {
+		int l = lcp_pos(p1, p2);
+		int min_len = std::min(len1, len2);
+		if (l >= min_len) {
+			return len1 < len2 ? -1 : (len1 == len2 ? 0 : 1);
+		}
+		return str[p1 + l] < str[p2 + l] ? -1 : 1;
+	}
+
+	// 求第k小(字典序)的本质不同子串
+	std::string kth_distinct_substring(i64 k) {
+		rep(i, 1, n) {
+			i64 cnt = (n - sa[i] + 1) - height[i];
+			if (k <= cnt) {
+				return str.substr(sa[i], height[i] + k);
+			}
+			k -= cnt;
+		}
+		return "";
 	}
 };
