27f4ee326625291530f59a82dd862e789d6a53bd
Aiza Lee <lee.aizazia@gmail.com>
2026-03-31 11:57:05 +0800

    update kmp

--- 字符串/KMP.cpp
+++ 字符串/KMP.cpp
@@ -9,8 +9,8 @@
  * 		无
  *
  * Interface:
- * 		KMP(s) / init(s): 初始化模式串
- * 		match(t): 在文本串中查找所有匹配位置，返回 1-based 起点
+ * 		KMP(sv) / init(sv): 初始化 0-based 模式串视图
+ * 		match(t): 传入 0-based 文本串视图，返回所有匹配的 1-based 起点
  * 		border_len(): 整个模式串的最长 border 长度
  * 		min_period(): 返回最小循环节长度
  * 		all_borders(): 返回整个模式串的所有 border 长度(降序)
@@ -19,30 +19,32 @@
  * Note:
  * 		1. Time: init O(M), match O(N)
  * 		2. Space: O(M)
- * 		3. 内部统一 1-based，模式串记为 s[1...m]，pi[i] 表示 s[1...i] 的最长 border 长度
- * 		4. 用法/技巧: 若 m % (m - pi[m]) == 0，则 m - pi[m] 是最小循环节
- * 		5. 相关笔记: 见 1-文字资料/字符串/弱周期分段与SeriesLink.tex
+ * 		3. 传入的 string_view 按 C++ 常规均为 0-based 视图
+ * 		4. 模式串以 string_view 保存，需保证底层串在 KMP 生命周期内有效
+ * 		5. 内部统一 1-based，模式串记为 s[1...m]，pi[i] 表示 s[1...i] 的最长 border 长度
+ * 		6. 用法/技巧: 若 m % (m - pi[m]) == 0，则 m - pi[m] 是最小循环节
+ * 		7. 相关笔记: 见 1-文字资料/字符串/弱周期分段与SeriesLink.tex
  */
 struct KMP {
-	std::string s;
+	std::string_view s;
 	int m = 0;
 	std::vector<int> pi;   // pi[i]: 前缀 s[1...i] 的最长 border 长度
 	std::vector<int> diff; // diff[len] = len - pi[len]
 	std::vector<int> anc;  // anc[len]: fail 树上跳到下一段等差数列的祖先
 
 	KMP() = default;
-	KMP(const std::string& s) { init(s); }
+	KMP(std::string_view s) { init(s); }
 
-	void init(const std::string& str) {
-		m = (int)str.size();
-		s = " " + str;
+	void init(std::string_view str) {
+		s = str;
+		m = (int)s.size();
 		pi.assign(m + 1, 0);
 		diff.assign(m + 1, 0);
 		anc.assign(m + 1, 0);
 
 		for (int i = 2, j = 0; i <= m; ++i) {
-			while (j > 0 && s[i] != s[j + 1]) j = pi[j];
-			if (s[i] == s[j + 1]) ++j;
+			while (j > 0 && s[i - 1] != s[j]) j = pi[j];
+			if (s[i - 1] == s[j]) ++j;
 			pi[i] = j;
 		}
 
@@ -52,15 +54,14 @@ struct KMP {
 		}
 	}
 
-	std::vector<int> match(const std::string& t) const {
+	std::vector<int> match(std::string_view t) const {
 		std::vector<int> occ;
 		int n = (int)t.size();
 		if (m == 0 || n == 0) return occ;
-		std::string tt = " " + t;
 
 		for (int i = 1, j = 0; i <= n; ++i) {
-			while (j > 0 && tt[i] != s[j + 1]) j = pi[j];
-			if (tt[i] == s[j + 1]) ++j;
+			while (j > 0 && t[i - 1] != s[j]) j = pi[j];
+			if (t[i - 1] == s[j]) ++j;
 			if (j == m) {
 				occ.emplace_back(i - m + 1);
 				j = pi[j];
